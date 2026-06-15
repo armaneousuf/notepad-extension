9328768d3fe0b8813bc588406a09655031d4b259
armaneousuf <armaneousuf97@gmail.com>
2026-06-16 01:41:05 +0600

    feat: added new icons

--- popup.js
+++ popup.js
@@ -813,7 +813,7 @@ function updateStats() {
 
   if (readingTime) {
     const minutes = Math.ceil(words / 200);
-    readingTime.textContent = `⏱️ ${words === 0 ? 0 : minutes}m read`;
+    readingTime.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" style="vertical-align: -0.15em; margin-right: 2px;"><title>clock</title><path fill="currentColor" d="M11.5 3a9.5 9.5 0 0 1 9.5 9.5a9.5 9.5 0 0 1-9.5 9.5A9.5 9.5 0 0 1 2 12.5A9.5 9.5 0 0 1 11.5 3m0 1A8.5 8.5 0 0 0 3 12.5a8.5 8.5 0 0 0 8.5 8.5a8.5 8.5 0 0 0 8.5-8.5A8.5 8.5 0 0 0 11.5 4M11 7h1v5.42l4.7 2.71l-.5.87l-5.2-3z"/></svg> ${words === 0 ? 0 : minutes}m read`;
   }
 
   const totalNotesCount = notes.length;
@@ -832,9 +832,9 @@ function updateStats() {
         ? (totalWordsCount / 1000).toFixed(1) + "kw"
         : totalWordsCount + "w";
 
-    globalStats.textContent = `📁 Total: ${totalNotesCount} note${
-      totalNotesCount !== 1 ? "s" : ""
-    } · ${wordDisplay}`;
+    globalStats.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" style="vertical-align: -0.15em; margin-right: 2px;"><title>file</title><path fill="currentColor" d="M14 11a3 3 0 0 1-3-3V4H7a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-8zm-2-3a2 2 0 0 0 2 2h3.59L12 4.41zM7 3h5l7 7v9a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3"/></svg> Total: ${totalNotesCount} note${
+  totalNotesCount !== 1 ? "s" : ""
+} · ${wordDisplay}`;
   }
 }
 
